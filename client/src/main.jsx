0fddf0d70bc90abf6d89dbb8b1b6d6d61e0e8a39
Jessica Morrison <jlmorrison@student.fullsail.edu>
2026-01-22 20:57:55 -0700

    updated css import in main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'; // React library
 import ReactDOM from 'react-dom/client'; // Renderer
 import App from './App'; // Root component
+import './styles.css';
 
 // Creates root and renders App component
 ReactDOM.createRoot(document.getElementById('root')).render(
